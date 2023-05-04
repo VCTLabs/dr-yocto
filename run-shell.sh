7383bfb8fdccbb3f07498aa998a5085c1b8c758f
Stephen L Arnold <sarnold@vctlabs.com>
2023-05-03 20:25:11 -0700

    new: usr: add Jammy as build target, cleanup ssh with github
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- run-shell.sh
+++ run-shell.sh
@@ -5,6 +5,9 @@
 #     ./run-shell.sh 18.04
 
 docker run -it --rm \
-  -e SSH_AUTH_SOCK=/tmp/ssh_auth.sock \
-  -v $SSH_AUTH_SOCK:/tmp/ssh_auth.sock \
+  -e "USER_ID=$(id -u)" \
+  -e GROUP_ID="$(id -g)" \
+  -v "$HOME/.ssh:/home/embeddeduse/.ssh:rw" \
+  -v "$SSH_AUTH_SOCK:/ssh.socket" \
+  -e "SSH_AUTH_SOCK=/ssh.socket" \
   -v $PWD:/public/workdir dr-yocto:$1
